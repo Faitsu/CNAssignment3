34198780eebac4fb4d29270b4b3acdea9476d30f
Erica Chou <emc689@nyu.edu>
2020-10-10 03:32:10 -0400

    Assignment 3

--- solution.py
+++ solution.py
@@ -86,5 +86,5 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
 
 
 if __name__ == '__main__':
-    #smtp_client(1025, '127.0.0.1')
-    smtp_client(25, 'smtp.nyu.edu')
+    smtp_client(1025, '127.0.0.1')
+    #smtp_client(25, 'smtp.nyu.edu')
